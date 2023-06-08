ae3b0d98220e08bd4ad845b60aada63f8837b10c
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-06-08 23:50:10 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
+let https = 'https://'
+let randLink = 'random link'
+let price = 33
+let imgUrl = `${https}image.link${randLink}`
+let link = `google.com`
+
+let Product = (props) => {
+  let {imgUrl, name, price, link} = props
+  return (
+    <div className={`block`}>
+      <h2>{props.name}</h2>
+      <img src={props.imgUrl} alt={props.imgUrl}/>
+      <a href={props.link} target={`_blank`}>Buy now</a>
+      <p>Price {props.price}</p>
+      <button type={"button"}>Get it</button>
+    </div>
+    )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Product</h1>
+      <Product imgUrl={``} name={`Kebab and lavash`} price={20} link={`google.com`}/>
+      {/*<Product/>*/}
+      {/*<Product/>*/}
+    </>
+  )
 }
 
 export default App;
