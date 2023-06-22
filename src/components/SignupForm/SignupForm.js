31c0ea43768fb98869a2d388899b9fade4a673e9
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-06-22 17:28:40 +0300

    form-end

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react'
+import {green}            from "@mui/material/colors"
+import Layout             from "../Layout"
+
+const Subscription = {
+  FREE: 'free',
+  PRO: 'pro',
+  PREMIUM: 'premium',
+}
 
 class SignupForm extends Component {
 
   state = {
     email: '',
     password: '',
+    subscription: 'free',
+    agreed: false,
+    age: '',
   }
 
   handleSubmit = e => {
     e.preventDefault()
-  }
-
-  handleEmailChange    = e => {
-    e.preventDefault()
-    this.setState({email: e.target.value})
-
-  }
-  handlePasswordChange = e => {
-    e.preventDefault()
-    this.setState({password: e.target.value})
-
   }
 
   handleChange = (e) => {
@@ -29,23 +29,82 @@ class SignupForm extends Component {
     })
   }
 
+  handleSubscriptionChange = (e) => {
+    this.setState({
+      subscription: e.target.value,
+    })
+  }
+
+  handleAgreedChange = e => {
+    console.log(`handleAgreedChange =>> `, e)
+    this.setState({
+      agreed: e.target.checked,
+    })
+  }
+
+  handleAgeChange = e => {
+    console.log(`handleAgeChange =>> `)
+    this.setState({
+      age: e.target.value,
+    })
+  }
+
   render() {
-    let {email, password} = this.state
+
+    let {email, password, subscription, agreed, age} = this.state
+
+    console.log(`agreed =>> `, agreed)
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Email
-          <input type="email" value={email} name={`email`} onChange={this.handleChange}/>
-        </label>
-        <br/>
-        <label>
-          Password
-          <input type="password" value={password} name={`password`} onChange={this.handleChange}/>
-        </label>
-        <br/>
-        <button type={`submit`}>Submit</button>
-      </form>
+      <Layout>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email
+            <input type="email" value={email} name={`email`} onChange={this.handleChange}/>
+          </label>
+          <br/>
+          <label>
+            Password
+            <input type="password" value={password} name={`password`} onChange={this.handleChange}/>
+          </label>
+          <br/>
+          <div role={`group`}>
+            <label>
+              Free
+              <input type="radio" value={Subscription.FREE} checked={Subscription.FREE === subscription}
+                     onChange={this.handleSubscriptionChange}/>
+            </label>
+            <label>
+              Pro
+              <input type="radio" value={Subscription.PRO} checked={Subscription.PRO === subscription}
+                     onChange={this.handleSubscriptionChange}/>
+            </label>
+            <label>
+              Premium
+              <input type="radio" value={Subscription.PREMIUM} checked={Subscription.PREMIUM === subscription}
+                     onChange={this.handleSubscriptionChange}/>
+            </label>
+          </div>
+          <br/>
+          <label>
+            Choose your age
+            <select name="age" value={age} onChange={this.handleAgeChange}>
+              <option value="">...</option>
+              <option value="18-26">18-26</option>
+              <option value="26-35">26-35</option>
+              <option value="36+">36+</option>
+            </select>
+          </label>
+          <br/>
+          <label>
+            Accept
+            <input type="checkbox" checked={agreed} onChange={this.handleAgreedChange}/>
+          </label>
+          <br/>
+          <button type={`submit`} disabled={!agreed}>Submit</button>
+        </form>
+      </Layout>
     )
   }
 }
