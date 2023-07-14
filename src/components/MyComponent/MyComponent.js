0ff21ce2b611c75f0359bd91c156de668c7dc43f
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-07-14 11:47:18 +0300

    toggler

--- src/components/MyComponent/MyComponent.js
+++ src/components/MyComponent/MyComponent.js
@@ -1,14 +1,13 @@
 import React                    from 'react'
-// import WithHigherOrderComponent from "../hoc/withHigherOrderComponent"
+import WithHigherOrderComponent from "../hoc/withHigherOrderComponent"
 // import WithLog                  from "../hoc/withLog"
-import WithFetch                from "../hoc/withFetch"
+// import WithFetch                from "../hoc/withFetch"
+import WithToggle from "../hoc/withToggle"
 
 const MyComponent = props => {
-  console.log(`MyComponent =>> OK`)
   return (
     <div>
-      <h1>MyComponent</h1>
-      {JSON.stringify(props, null, 2)}
+      <div>{JSON.stringify(props, null, 2)}</div>
     </div>
   )
 }
@@ -16,4 +15,6 @@ const MyComponent = props => {
 // export default WithHigherOrderComponent(MyComponent)
 // export default WithLog(MyComponent)
 let url = 'https://jsonplaceholder.typicode.com/todos/?userId=3'
-export default WithFetch(url)(MyComponent)
+// export default WithFetch(url)(MyComponent)
+
+export default WithToggle(MyComponent)
--- src/components/hoc/withToggle.js
+++ src/components/hoc/withToggle.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react'
 
-const WithToggle = WrappedComponent => {
+const withToggle = WrappedComponent => {
   return class WithToggle extends Component {
-    state  = {isOpen: false}
+    state = {
+      isOpen: false,
+    }
 
-    toggle = () => this.setState(prevState => ({isOpen: !prevState.isOpen}))
+    toggle = () => {
+      return this.setState(prevState => ({isOpen: !prevState.isOpen}))
+    }
 
     render() {
       return (
-        <>
-          <button type={`button`}>{this.state.isOpen ? 'Hide' : 'Show'}</button>
-          
+        <div>
+          <button type={`button`} onClick={this.toggle}>
+            {' '}
+            {this.state.isOpen ? 'Hide' : 'Show'}
+            {' '}
+          </button>
           <WrappedComponent {...this.props}/>
-        </>
+        </div>
       )
     }
   }
 }
 
-export default WithToggle
+export default withToggle
